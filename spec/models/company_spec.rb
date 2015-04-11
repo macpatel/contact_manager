53f50008608b7d79b2867956d354d72a28a1d8e4
Mahesh Patel <patel.mahesh034@gmail.com>
2015-04-11 05:07:03 +0000

    sync

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Company, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) { Company.create(name:'ABC') }
+  
+  it 'is valid' do
+      expect(company).to be_valid
+  end
+  
+  it 'is invalid without name' do
+      company.name = nil
+      expect(company).not_to be_valid
+  end
 end
