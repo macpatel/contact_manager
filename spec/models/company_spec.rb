1e0182ad1dd1b81ca8fa56a6afaec6a912d6db47
Mahesh Patel <patel.mahesh034@gmail.com>
2015-04-11 10:39:50 +0530

    sync

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Company, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) do
+		Company.new(name: 'company1', contact_id: 1, contact_type: 'Person')
+	end
+
+	it 'is valid' do
+		expect(company).to be_valid
+	end
 end
